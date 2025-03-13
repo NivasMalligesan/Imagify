5458c81f0b79978d99506a951c642bacaddd33d4
Nivas Malligesan <nivasmalligesan23@gmail.com>
2025-03-13 10:26:31 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,10 +7,26 @@ const Footer = () => {
     <div className='flex items-center justify-between 
     gap-4 py-5 mt-20'>
         <img src={assets.logo} alt="" width={150}/>
+        <div>
+          <h1 className='text-xl'>
+          Created By
+          <span className='bg-black py-2 px-2 rounded-xl ml-2 text-white '>
+           Nivas Malligesan
+          </span>
+          </h1>
+        </div>
+
         <div className='flex gap-4'>
+          <a href="https://www.linkedin.com/in/nivas-malligesan/"> 
             <FaLinkedinIn  size={25}/>
+          </a>
+          <a href="https://github.com/NivasMalligesan">
             <FaGithub size={25}/>
+          </a>
+          <a href="https://www.instagram.com/nivas_ig?igsh=MWVhejQ2OXM2a2F3eQ==">
             <FaInstagram  size={25}/>
+          </a>
+
         </div>
     </div>
   )
